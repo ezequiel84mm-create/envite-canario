c831ab684e709712002df17c0891f808d4805e80
Ezequiel Mejias Marcelino <ezequielmejiasmarcelino@MacBook-Air-de-Ezequiel.local>
2026-06-21 22:34:32 +0100

    2vs2 Pieza 4: repartidor para 4 jugadores con vira

---
+++ lib/features/multiplayer/domain/engine/deal_engine_2v2.dart
@@ -0,0 +1,63 @@
+import '../../../../core/enums/suit.dart';
+import '../../../../core/utils/deck_generator.dart';
+import '../../../game/data/models/card_model.dart';
+
+/// Resultado de un reparto de 2vs2: las manos de los 4 jugadores,
+/// la carta virada (define el triunfo) y el mazo restante.
+class Reparto2v2 {
+  /// Manos por asiento: manos[0] es la mano del asiento 0, etc.
+  final List<List<CardModel>> manos;
+
+  /// La carta virada (boca arriba), define el palo de triunfo.
+  final CardModel vira;
+
+  /// Palo de triunfo (el de la vira).
+  Suit get paloVirado => vira.suit;
+
+  /// Cartas que quedan en el mazo tras repartir y voltear la vira.
+  final List<CardModel> resto;
+
+  Reparto2v2({
+    required this.manos,
+    required this.vira,
+    required this.resto,
+  });
+}
+
+/// Repartidor para el modo 2vs2.
+class DealEngine2v2 {
+  /// Número de jugadores en 2vs2.
+  static const int numJugadores = 4;
+
+  /// Cartas que recibe cada jugador.
+  static const int cartasPorJugador = 3;
+
+  /// Reparte una mano nueva de 2vs2.
+  ///
+  /// Da 3 cartas a cada uno de los 4 jugadores y voltea la siguiente
+  /// carta como vira (triunfo).
+  static Reparto2v2 repartir() {
+    final mazo = DeckGenerator.generateShuffledDeck();
+
+    // Preparar una lista de manos vacías, una por asiento.
+    final manos = List.generate(numJugadores, (_) => <CardModel>[]);
+
+    int indice = 0;
+    // Repartir 3 cartas a cada jugador.
+    for (int ronda = 0; ronda < cartasPorJugador; ronda++) {
+      for (int asiento = 0; asiento < numJugadores; asiento++) {
+        manos[asiento].add(mazo[indice]);
+        indice++;
+      }
+    }
+
+    // La siguiente carta es la vira.
+    final vira = mazo[indice];
+    indice++;
+
+    // El resto queda en el mazo.
+    final resto = mazo.sublist(indice);
+
+    return Reparto2v2(manos: manos, vira: vira, resto: resto);
+  }
+}
